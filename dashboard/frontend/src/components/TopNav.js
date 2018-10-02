faca8d6688288ae3d4479684ba08eed4c9aca62b
Olga Belavina <ol.belavina@gmail.com>
2018-10-02 17:01:33 -0400

    Added status bar, updated material-ui to 3.x

--- dashboard/frontend/src/components/TopNav.js
+++ dashboard/frontend/src/components/TopNav.js
@@ -3,15 +3,19 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 // Material imports
+import { withStyles } from '@material-ui/core/styles';
+import { Settings, AcUnit, PowerSettingsNew, ArrowDownward } from "@material-ui/icons";
+// import s from "@material-ui/icons/ArrowUpward";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import SettingsIcon from "@material-ui/icons/Settings";
+import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import {Divider, Drawer} from '@material-ui/core';
+import { Divider, Drawer, FormControlLabel, Switch } from '@material-ui/core';
+
 
 class TopNav extends React.Component {
 
@@ -39,6 +43,7 @@ class TopNav extends React.Component {
     const drawerOpen = Boolean(drawerAnchor);
 
     return (
+      <div >
       <AppBar
         position="absolute"
         className={classNames(classes.appBar, classes[`appBar-left`])}
@@ -47,17 +52,17 @@ class TopNav extends React.Component {
           <Typography variant="title" color="inherit" noWrap>
             HAos Simulation Engine
           </Typography>
-          <div>
+          <div style={styles.grow} >
             <IconButton aria-owns={drawerOpen ? 'menu-appbar' : null}
                 aria-haspopup="true"
                 color="inherit"
                 onClick={this.handleMenu}
             >
-            <SettingsIcon/>
+            <Settings/>
             </IconButton>
-              <Drawer open={drawerOpen} onClose={this.handleDrawerClose}
+            <Drawer open={drawerOpen} onClose={this.handleDrawerClose}
                 classes={{paper: classes.drawerPaper,}} anchor={'left'}>
-                <div className={classes.toolbar}/>
+              <div className={classes.toolbar}/>
                 <Divider />
                 <div
                   tabIndex={0}
@@ -72,14 +77,102 @@ class TopNav extends React.Component {
                       </ListItem>
                     </List>
                   </div>
-                </div>
+              </div>
             </Drawer>
           </div>
+          <div>
+          <Grid container>
+            <Grid item >
+              <FormControlLabel
+                control={
+                  <Switch 
+                    checked={false} 
+                   
+                    classes={{
+                      switchBase: classes.colorSwitchBase,
+                      checked: classes.colorChecked,
+                      bar: classes.colorBar,
+                    }}
+                    aria-label="PowerSwitch"
+                  />
+                }
+                label={
+                  <Typography variant="title" style={{color: 'white'}}>
+                    <PowerSettingsNew style={styles.inlineIcon} />
+                      The Mains: <span  style={styles.heating}> offline</span>
+                  </Typography>
+                }
+              />
+            </Grid> 
+            <Grid item style={{...styles.menuOptions, ...styles.tempGauge}}>
+              <Typography variant="title" color="inherit" >
+              
+                <AcUnit style={styles.inlineIcon}/>
+             
+                <span style={styles.cooling}>21° <ArrowDownward style={styles.inlineIcon}/></span> 
+              </Typography>
+            </Grid>
+          </Grid>
+          </div>
+
         </Toolbar>
       </AppBar>
+      </div>
     );
   }
 }
+
+const colors = {
+  on: '#27ae60',
+  off: '#e74c3c',
+  cooling: '#3498db',
+}
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  inlineIcon: {
+    marginRight: '0.3em',
+    marginBottom: '-0.2em',
+    fontSize: 22
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  colorSwitchBase: {
+    color: colors.off,
+    '&$colorChecked': {
+      color: colors.on,
+      '& + $colorBar': {
+        backgroundColor: colors.on,
+      },
+    },
+  },
+  colorBar: {},
+  colorChecked: {},
+  cooling: {
+    color: colors.cooling
+  },
+  heating: {
+    color: colors.off
+  },
+  online: {
+    color: colors.on
+  },
+  rightMenuContainer: {
+    display: 'flex',
+    direction: 'column'
+  },
+  menuOptions: {
+    padding: '0.7em',
+  },
+  tempGauge: {
+    borderColor:"white", 
+    borderRightStyle: 'solid',
+    borderLeftStyle: 'solid',
+  }
+};
 
 TopNav.propTypes = {
   classes: PropTypes.object, // styling
@@ -87,4 +180,4 @@ TopNav.propTypes = {
 };
 
 
-export default TopNav;
+export default  withStyles(styles)(TopNav);
