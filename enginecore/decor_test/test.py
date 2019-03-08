3e0137c83725491c73bad3aa800e8ed5c1e0f0f9
Olga Belavina <ol.belavina@gmail.com>
2019-03-08 13:08:49 -0500

    Added timestamp recorder

--- enginecore/decor_test/test.py
+++ enginecore/decor_test/test.py
@@ -1,6 +1,10 @@
 # pylint: skip-file
 
 import functools
+from itertools import zip_longest
+from datetime import datetime as dt
+import time
+
 
 
 class Recorder:
@@ -9,20 +13,29 @@ class Recorder:
     def __init__(self):
         self._actions = []
 
+
     def __call__(self, f):
         @functools.wraps(f)
         def record_wrapper(asset_self, *f_args, **f_kwargs):
-            self._actions.append(functools.partial(f, asset_self, *f_args, **f_kwargs))
-            
+            self._actions.append({
+                'work': functools.partial(f, asset_self, *f_args, **f_kwargs),
+                'timestamp': dt.now()
+            })
             return f(asset_self, *f_args, **f_kwargs)
         return record_wrapper
+
 
     def replay_all(self):
         self.replay_range(slice(None, None))
 
-    def replay_range(self, slc):
-        [a() for a in self._actions[slc]]
 
+    def replay_range(self, slc):
+
+        for action, next_action in zip_longest(self._actions[slc], self._actions[1:][slc]):
+
+            action['work']()
+            if next_action:
+                time.sleep((next_action['timestamp'] - action['timestamp']).seconds)
 
 
 recorder = Recorder()
@@ -43,12 +56,15 @@ class Server:
 
 
 server = Server()
-# server.power_down()
-# server.power_up()
-server.power_down()
+server.power_down() # <-takes 1 second
+
+time.sleep(5)
+server.power_up()
 server.destroy_fan(3)
 
 
 print('== Replaying actions: ==')
-recorder.replay_all()
-# recorder.replay_range(slice(-2, None))
+# recorder.replay_all()
+
+play_last_2 = slice(-2, None)
+recorder.replay_range(play_last_2)
