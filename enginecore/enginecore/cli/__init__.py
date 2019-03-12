e6c19dcbf80b3880a35764769446ad8b977eafc8
Olga Belavina <ol.belavina@gmail.com>
2019-03-12 11:35:12 -0400

    Grouped cli modules

---
+++ enginecore/enginecore/cli/__init__.py
@@ -0,0 +1,7 @@
+from enginecore.cli.status import status_command
+from enginecore.cli.power import power_command
+from enginecore.cli.thermal import thermal_command
+from enginecore.cli.configure_state import configure_command
+from enginecore.cli.model import model_command
+from enginecore.cli.storage import storage_command
+from enginecore.cli.play import play_command
