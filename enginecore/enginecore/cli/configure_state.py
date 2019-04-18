8fca41fdf11053b6fa5e08b274410368c8f197bf
Olga Belavina <ol.belavina@gmail.com>
2019-04-18 10:21:07 -0400

    Added list randoptions CLI command

--- enginecore/enginecore/cli/configure_state.py
+++ enginecore/enginecore/cli/configure_state.py
@@ -82,66 +82,107 @@ def configure_command(configure_state_group):
     )
 
     conf_rand_action.add_argument(
-        "-s",
-        "--start",
-        type=int,
-        required=True,
-        help="Start range value for randomized option",
+        "-s", "--start", type=int, help="Start range value for randomized option"
     )
 
     conf_rand_action.add_argument(
-        "-e",
-        "--end",
-        type=int,
-        required=True,
-        help="End range value for randomized option",
+        "-e", "--end", type=int, help="End range value for randomized option"
     )
+
+    conf_rand_action.add_argument(
+        "-l",
+        "--list",
+        action="store_true",
+        help="List randranges for ranomizable options",
+    )
+
+    conf_rand_arguments = [
+        "pd-media-error-count",
+        "pd-other-error-count",
+        "pd-predictive-error-count",
+        "ctrl-memory-correctable-errors",
+        "ctrl-memory-uncorrectable-errors",
+        "ambient",
+    ]
 
     conf_rand_action.add_argument(
         "-o",
         "--option",
-        required=True,
         help="Option/Argument to be configured",
-        choices=[
-            "pd-media-error-count",
-            "pd-other-error-count",
-            "pd-predictive-error-count",
-            "ctrl-memory-correctable-errors",
-            "ctrl-memory-uncorrectable-errors",
-            "ambient",
-        ],
+        choices=conf_rand_arguments,
     )
 
     conf_rand_action.set_defaults(validate=validate_randomizer_options)
 
-    conf_rand_action.set_defaults(func=handle_configure_randomizer)
+    conf_rand_action.set_defaults(
+        func=lambda args: handle_configure_randomizer(args, conf_rand_arguments)
+    )
 
 
 def validate_randomizer_options(args):
-    """Check if asset key was supplied for some options"""
-    if args["option"] != "ambient" and not args["asset_key"]:
+    """Check if asset key was supplied for some options (options for disk errors)"""
+
+    if args["option"] and args["option"] != "ambient" and not args["asset_key"]:
         raise argparse.ArgumentTypeError(
             'Asset key is required for "{option}" option!'.format(**args)
         )
 
 
-def handle_configure_randomizer(args):
+def handle_configure_randomizer(args, conf_rand_arguments):
     """Callback for CLI command for configuring randomizer"""
 
+    # list all the randoptions
+    if args["list"]:
+        _, amb_args = ISystemEnvironment.get_ambient_props()
+
+        print(
+            "Ambient random arguments: range from {start} to {end}".format(**amb_args)
+        )
+
+        if not args["asset_key"]:
+            return
+
+        del conf_rand_arguments[conf_rand_arguments.index("ambient")]
+
+        server_manager = get_server_state_manager(args["asset_key"])
+        print("Server:[{0.key}] random arguments:".format(server_manager))
+        for rand_opt in conf_rand_arguments:
+
+            s_prop = IBMCServerStateManager.StorageRandProps[rand_opt.replace("-", "_")]
+            opt_prop = server_manager.get_storage_radnomizer_prop(s_prop)
+            print(
+                " -- {}: range from {} to {}".format(rand_opt, opt_prop[0], opt_prop[1])
+            )
+        return
+
+    # configure randargs - validate cli options
+    if not args["start"] or not args["end"] or not args["option"]:
+        raise argparse.ArgumentTypeError(
+            "Must provide rand option, start & end range values!"
+        )
+
+    # update ambient option
     if args["option"] == "ambient":
         ISystemEnvironment.set_ambient_props(args)
         return
 
-    server_manager = IStateManager.get_state_manager_by_key(args["asset_key"])
-    if not isinstance(server_manager, IBMCServerStateManager):
-        raise argparse.ArgumentTypeError(
-            "Asset [{}] is not a server!".format(args["asset_key"])
-        )
-
+    # update one of the server disk options
+    server_manager = get_server_state_manager(args["asset_key"])
     server_manager.set_storage_randomizer_prop(
         IBMCServerStateManager.StorageRandProps[args["option"].replace("-", "_")],
         slice(args["start"], args["end"]),
     )
+
+
+def get_server_state_manager(asset_key):
+    """Get server asset by key with type validation"""
+
+    server_manager = IStateManager.get_state_manager_by_key(asset_key)
+    if not isinstance(server_manager, IBMCServerStateManager):
+        raise argparse.ArgumentTypeError(
+            "Asset [{}] is not a server!".format(asset_key)
+        )
+    return server_manager
 
 
 def configure_battery(key, kwargs):
