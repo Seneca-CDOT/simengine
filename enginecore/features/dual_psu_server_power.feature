c1769942b0d1a573097e25e8b15524b687d5c456
Olga Belavina <ol.belavina@gmail.com>
2019-06-24 11:51:47 -0400

    Grouped together model related steps

--- enginecore/features/dual_psu_server_power.feature
+++ enginecore/features/dual_psu_server_power.feature
@@ -4,5 +4,12 @@ Feature: Dual PSU Voltage Handling
 
     Scenario: Load is correctly spread over 2 PSUs when both power sources are present
         Given the system model is empty
+
+        # initialize model
+        And Outlet asset with key "1" is created
+        And Outlet asset with key "2" is created
         And Server asset with key "180", "2" PSU(s) and "480" Wattage is created
+        And asset "1" powers target "1801"
+        And asset "2" powers target "1802"
+
         And Engine is up and running
--- enginecore/features/steps/model.py
+++ enginecore/features/steps/model.py
@@ -4,8 +4,52 @@ from behave import given, when, then, step
 # plyint: enable=no-name-in-module
 
 import enginecore.model.system_modeler as sm
+from enginecore.state.api.state import IStateManager
+
+TEST_VM_NAME = "an-a01n01"
 
 
 @given("the system model is empty")
-def step_impl(_):
+def step_impl(context):
     sm.drop_model()
+    context.hardware = {}
+
+
+@given('Outlet asset with key "{key:d}" is created')
+def step_impl(context, key):
+    sm.create_outlet(key, {})
+    context.hardware[key] = IStateManager.get_state_manager_by_key(key)
+
+
+@given(
+    'Server asset with key "{key:d}", "{psu_num:d}" PSU(s) and "{wattage:d}" Wattage is created'
+)
+def step_impl(context, key, psu_num, wattage):
+
+    sm.create_server(
+        key,
+        {
+            "domain_name": TEST_VM_NAME,
+            "psu_num": psu_num,
+            "psu_load": [0.5, 0.5],
+            "power_consumption": wattage,
+            "psu_power_consumption": 0,
+            "psu_power_source": 120,
+        },
+    )
+
+    context.hardware[key] = IStateManager.get_state_manager_by_key(key)
+    psu_key_1 = key * 10 + 1
+    psu_key_2 = key * 10 + 2
+
+    context.hardware[psu_key_1] = IStateManager.get_state_manager_by_key(psu_key_1)
+    context.hardware[psu_key_2] = IStateManager.get_state_manager_by_key(psu_key_2)
+
+
+@given('asset "{source_key:d}" powers target "{dest_key:d}"')
+def step_impl(context, source_key, dest_key):
+
+    assert source_key in context.hardware
+    assert dest_key in context.hardware
+
+    sm.link_assets(source_key, dest_key)
