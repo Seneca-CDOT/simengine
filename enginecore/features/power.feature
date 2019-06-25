94224cee44e1bd008eec3c2094e0736168149c30
Olga Belavina <ol.belavina@gmail.com>
2019-06-25 14:45:19 -0400

    Implemented 3 more scenarios for simple lamp topology

--- enginecore/features/power.feature
+++ enginecore/features/power.feature
@@ -1,6 +1,7 @@
 Feature: Power Chaining for Hardware Assets
     Simple 2-asset power scenarios involving a lamp and an outlet powering it;
 
+    @power-behaviour
     Scenario: Lamp is on and it's drawing power
         Given the system model is empty
 
@@ -11,8 +12,69 @@ Feature: Power Chaining for Hardware Assets
 
         And Engine is up and running
 
+        # check state
         Then asset "1" load is set to "1.0"
         And asset "2" load is set to "1.0"
         And asset "1" is online
         And asset "2" is online
+
+    @power-behaviour
+    Scenario: Lamp goes offline
+        Given the system model is empty
+
+        # initialize model
+        And Outlet asset with key "1" is created
+        And Lamp asset with key "2", minimum "109" Voltage and "120" Wattage is created
+        And asset "1" powers target "2"
+
+        And Engine is up and running
+
+        When asset "2" is powered down
+
+        # check state
+        Then asset "1" load is set to "0.0"
+        And asset "2" load is set to "0.0"
+        And asset "1" is online
+        And asset "2" is offline
+
+    @power-behaviour
+    Scenario: Lamp goes offline and back online
+        Given the system model is empty
+
+        # initialize model
+        And Outlet asset with key "1" is created
+        And Lamp asset with key "2", minimum "109" Voltage and "120" Wattage is created
+        And asset "1" powers target "2"
+
+        And Engine is up and running
+
+        When asset "2" is powered down
+        And asset "2" is powered up
+
+        # check state
+        Then asset "1" load is set to "1.0"
+        And asset "2" load is set to "1.0"
+        And asset "1" is online
+        And asset "2" is online
+
+
+    @power-behaviour
+    Scenario: Outlet goes offline
+        Given the system model is empty
+
+        # initialize model
+        And Outlet asset with key "1" is created
+        And Lamp asset with key "2", minimum "109" Voltage and "120" Wattage is created
+        And asset "1" powers target "2"
+
+        And Engine is up and running
+
+        When asset "1" is powered down
+
+        # check state
+        Then asset "1" load is set to "0.0"
+        And asset "2" load is set to "0.0"
+        And asset "1" is offline
+        And asset "2" is offline
+
 
--- enginecore/features/steps/power.py
+++ enginecore/features/steps/power.py
@@ -1,8 +1,89 @@
 # pylint: disable=no-name-in-module,function-redefined,missing-docstring,unused-import
+import logging
+import os
+import queue
+import sys
+from threading import Thread
+
 from behave import given, when, then, step
 from hamcrest import *
 
+from enginecore.state.net.ws_requests import ServerToClientRequests
+import enginecore.state.state_initializer as state_ini
+from enginecore.state.engine import Engine
+
+from test_helpers import TestClient
+
 # plyint: enable=no-name-in-module
+def configure_logger():
+    log_format = "[%(threadName)s, %(asctime)s, %(module)s:%(lineno)s] %(message)s"
+
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
+    formatter = logging.Formatter(log_format)
+
+    stdout_h = logging.StreamHandler(sys.stdout)
+    stdout_h.setFormatter(formatter)
+
+    root.addHandler(stdout_h)
+
+
+@given("Engine is up and running")
+def step_impl(context):
+
+    configure_logger()
+
+    os.environ["SIMENGINE_WORKPLACE_TEMP"] = "simengine-test"
+
+    state_ini.configure_env(relative=True)
+    state_ini.initialize(force_snmp_init=True)
+
+    if "engine" in context:
+        print(context.engine)
+    # Start up simengine
+    context.engine = Engine()
+    context.engine.start()
+
+    context.ws_queue = queue.Queue()
+    context.ws_client = TestClient()
+
+    TestClient.context = context
+    TestClient.queue = context.ws_queue
+
+    TestClient.ws_thread = Thread(
+        target=context.ws_client.run_client, name="engine-client"
+    )
+    TestClient.ws_thread.daemon = True
+    TestClient.ws_thread.start()
+
+    loop_done_num = 0
+    num_mains_outs = len(context.engine.mains_out_keys)
+
+    while loop_done_num != num_mains_outs or not context.ws_queue.empty():
+
+        message = context.ws_queue.get()
+        logging.info("Consumer storing message: %s (size=%d)", message, num_mains_outs)
+
+        if message["request"] == ServerToClientRequests.load_loop_done.name:
+            loop_done_num = loop_done_num + 1
+
+
+@when('asset "{key:d}" is powered down')
+def step_impl(context, key):
+    context.hardware[key].shut_down()
+    context.engine.handle_state_update(key, 0)
+    import time
+
+    # time.sleep(2)
+
+
+@when('asset "{key:d}" is powered up')
+def step_impl(context, key):
+    context.hardware[key].power_up()
+    context.engine.handle_state_update(key, 1)
+    import time
+
+    # time.sleep(2)
 
 
 @then('asset "{key:d}" load is set to "{load:f}"')
