eafb7288f8ca8e73a5bd60f9d9235e598693cc6d
Tanner Moss <tmoss404@gmail.com>
2022-11-11 14:32:00 -0500

    changed to logging specifically UPS #3 AssetLoadEvents

--- enginecore/enginecore/state/net/ws_server.py
+++ enginecore/enginecore/state/net/ws_server.py
@@ -390,9 +390,12 @@ class WebSocket(Component):
         if not event:
             return
 
-        logger.info("event: %s", event)
+        if event._asset.key == 3:
+            logger.info("event: %s", event)
+
         if not event.load.unchanged():
-            logger.info("\n!!!-------------------AssetLoadEvent (LOAD CHANGE) --------------------!!!")
+            if event._asset.key == 3:
+                logger.info("\n!!!-------------------AssetLoadEvent (LOAD CHANGE) --------------------!!!")
             client_request = ServerToClientRequests.asset_upd
             payload = {"key": event.asset.key, "load": event.load.new}
 
